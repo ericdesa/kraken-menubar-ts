bebe43292e9a38bf0ed8f2619b9c20d02c1b6f3d
Eric <eric@frianbiz.com>
2017-12-17 23:10:05 +0100

    refactor(navigation): remove router & setup-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
@@ -15,13 +14,6 @@ import { SetupPageComponent } from 'app/pages/setup/setup.page.component';
 import { KrakenService } from 'app/services/kraken/kraken.service';
 
 // libs
-import { MatButtonModule, MatInputModule, MatMenuModule, MatToolbarModule } from '@angular/material';
-import { HttpClientModule } from '@angular/common/http';
-
-const appRoutes: Routes = [
-    { path: 'setup', component: SetupPageComponent },
-    { path: '**', component: HomePageComponent },
-];
 
 @NgModule({
     declarations: [
@@ -29,21 +21,14 @@ const appRoutes: Routes = [
 
         // pages
         HomePageComponent,
-        SetupPageComponent,
     ],
     imports: [
         // angular
         BrowserModule,
         BrowserAnimationsModule,
         FormsModule,
-        RouterModule.forRoot(appRoutes),
         HttpClientModule,
 
-        // Angular Material
-        MatButtonModule,
-        MatInputModule,
-        MatToolbarModule,
-        MatMenuModule
     ],
     entryComponents: [
     ],
